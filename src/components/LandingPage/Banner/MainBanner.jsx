9898b67edb3c51d2df4659a05773832f57e90824
ABDUL MAJID K <majidmaji3410@gmail.com>
2024-03-29 14:21:56 +0530

    Banner text update

--- src/components/LandingPage/Banner/MainBanner.jsx
+++ src/components/LandingPage/Banner/MainBanner.jsx
@@ -1,31 +1,56 @@
-import React from 'react'
-import ban1 from '../../../assets/ban1.png'
-import ban2 from '../../../assets/ban2.png'
-import logo from '../../../assets/logo.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import ban1 from "../../../assets/ban1.png";
+import ban2 from "../../../assets/ban2.png";
+import logo from "../../../assets/logo3.png";
+import logo1 from "../../../assets/logo.png";
+
+import { Link } from "react-router-dom";
 export default function MainBanner() {
   return (
     <>
-          <div className='md:hidden fixed bg-black h-[800px] w-full block'>
-          <img className='w-full h-full object-cover' src={ban2} alt="MainBanner" />
-          <div className='absolute '>
-            <img className='w-40 -mt-[560px] ml-28 h-full object-cover' src={logo} alt="BrandLogo" />
-            <Link to={'/product/new-in'}>
-            <h1 className='text-white font-roboto text-xs cursor-pointer mt-72 ml-44 '>SHOP ALL</h1>
+      <div className="md:hidden fixed bg-black h-[800px] w-full block">
+        <img
+          className="w-full h-full object-cover"
+          src={ban2}
+          alt="MainBanner"
+        />
+        <div className="flex justify-center">
+        <div className="absolute ">
+          <img
+            className="w-40 -mt-[560px]  h-full object-cover"
+            src={logo}
+            alt="BrandLogo"
+          />
+          <Link to={"/product/new-in"}>
+            <h1 className="text-white font-roboto text-xs cursor-pointer mt-80 ml-12 ">
+              SHOP ALL
+            </h1>
+          </Link>
+        </div>
+        </div>
+     
+      </div>
+      <div className="md:block fixed   bg-black h-[650px]  w-full  hidden">
+        <img
+          className="w-full h-full object-cover"
+          src={ban1}
+          alt="MainBanner"
+        />
+        <div className="flex justify-center">
+          <div className="absolute  ">
+            <img
+              className="w-40 -mt-[350px]   h-full object-cover"
+              src={logo1}
+              alt="BrandLogo"
+            />
+            <Link to={"/product/new-in"}>
+              <h1 className="text-black font-roboto text-xs cursor-pointer mt-48 ml-12  ">
+                SHOP ALL
+              </h1>
             </Link>
-           
           </div>
-          </div>
-          <div className='md:block fixed   bg-black h-[650px]  w-full  hidden'>
-          <img className='w-full h-full object-cover' src={ban1} alt="MainBanner" />
-          <div className='absolute '>
-            <img className='w-40 -mt-[350px] ml-96 h-full object-cover' src={logo} alt="BrandLogo" />
-            <Link to={'/product/new-in'}>
-            <h1 className='text-black font-roboto text-xs cursor-pointer mt-72 ml-44 '>SHOP ALL</h1>
-            </Link>
-           
-          </div>
-          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
